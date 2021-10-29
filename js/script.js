0c4ff3adfe629247050fd7b33f9727f0ed50fa69
Leticia <leticiancalves1@gmail.com>
2021-10-29 02:09:35 -0300

    adicionada paginacao

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-import {Slide, SlideNav} from './slide.js';
+import { SlideNav } from './slide.js';
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
-slide.addArrow('.prev', '.next')
+slide.addArrow('.prev', '.next');
 
-slide.changeSlide(3);
-slide.activePrevSlide();
+slide.addControl();
+
+console.log(slide);
